4953574fe492d9786849d3fdb1c553685b74ba4f
vnica2 <116927189+vnica2@users.noreply.github.com>
2022-11-30 22:27:46 -0600

    Add files via upload

--- bot/main.py
+++ bot/main.py
@@ -6,11 +6,15 @@ import sys
 import random
 print("    $   $  RUNNING")
 sys.stdout.flush()
-USER_AGENTS = ["Mozilla/5.0 (iPhone; CPU iPhone OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Mobile/15E148 Safari/604.1","Mozilla/5.0 (iPhone; CPU iPhone OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/107.0.5304.66 Mobile/15E148 Safari/604.1","Mozilla/5.0 (iPhone; CPU iPhone OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/106.0 Mobile/15E148 Safari/605.1.15"]
-
+USER_AGENTS = ["Mozilla/5.0 (iPhone; CPU iPhone OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Mobile/15E148 Safari/604.1","Mozilla/5.0 (iPhone; CPU iPhone OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/107.0.5304.66 Mobile/15E148 Safari/604.1","Mozilla/5.0 (iPhone; CPU iPhone OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/106.0 Mobile/15E148 Safari/605.1.15","Mozilla/5.0 (iPad; CPU OS 15_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148","Mozilla/5.0 (iPad; CPU OS 15_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"    ,"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36","Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"]
+selection = 0
 def download_and_decode_html(url):
     websiteurl = "https://sci-hub.se/" + url
-    req = request.Request(url=websiteurl, headers={'User-Agent' : USER_AGENTS[random.randrange(3)]})
+    global selection
+    print("[1] selection was... ", selection)
+    selection = (selection + 1) % len(USER_AGENTS)
+    print(" selection is now... ", selection)
+    req = request.Request(url=websiteurl, headers={'User-Agent' : USER_AGENTS[selection]})
     fp = request.urlopen(req)
     mybytes = fp.read()
     mystr = mybytes.decode("utf8")
@@ -51,7 +55,7 @@ def download_file(url):
     else:
         downloadurl = "https://sci-hub.se/" + downloadurl
     sys.stdout.flush()
-    print("   downloadurl" + downloadurl)
+    print("   downloadurl: " + downloadurl)
     sys.stdout.flush()
 
     #get doi
@@ -62,7 +66,7 @@ def download_file(url):
             break
         doi += i
     doi = doi.strip()
-    print("  doi" + doi)
+    print("  doi: " + doi)
     sys.stdout.flush()
 
     #delete illegal filename characters
@@ -80,7 +84,11 @@ def download_file(url):
     sys.stdout.flush()
     #download and return
     opener = request.build_opener()
-    opener.addheaders = [('User-Agent',USER_AGENTS[random.randrange(3)])]
+    global selection
+    print("[1] selection was... ", selection)
+    selection = (selection + 1) % len(USER_AGENTS)
+    print(" selection is now... ", selection)
+    opener.addheaders = [('User-Agent',USER_AGENTS[selection])]
     request.install_opener(opener)
     return request.urlretrieve(downloadurl, filename)[0],doi
 
@@ -90,6 +98,8 @@ def download_file(url):
 def url_conform(url):
     con_url = url
     con_url = con_url.strip()
+    if con_url[:4] == "10.1" or con_url[:4] == "doi/":
+        return "https://doi.org/" + con_url
     if con_url[:7] != "http://":
         if con_url[:8] == "https://":
             return con_url
@@ -125,10 +135,12 @@ async def on_message(message):
         return
 
     if message.content.startswith('$sh '):
+        print("\n =-=-=-=-=-=-=-=-=-=-")
         print("recieved message: " + message.content)
         sys.stdout.flush()
         await message.add_reaction("👍")
         url = url_conform(message.content[4:])
+        print(url)
         try:
             contents = download_file(url)
             filename = contents[0]
@@ -137,7 +149,7 @@ async def on_message(message):
                 print("sent file")
                 sys.stdout.flush()
             if os.path.exists(filename):
-                print("attempted to remove file" + filename)
+                print("attempted to remove file " + filename)
                 sys.stdout.flush()
                 os.remove(filename)
                 print("current pwd contents: " + str(os.listdir()))
@@ -150,12 +162,16 @@ async def on_message(message):
             sys.stdout.flush()
             print("general error")
             sys.stdout.flush()
-            await message.channel.send("there was an error :(. malformed message or paper not found on sci-hub.se. if it is, dm the link to @buck#9576 so I can fix the bot :)")
+            await message.channel.send("error;; paper might not be found on sci-hub.se. if it is, dm the link to @buck#9576 and i will try to fix bot")
+            await message.channel.send('''either put the url of the paper like so:`$sh urlofpaper`
+OR put the doi like so: `$sh 10.xxxx/blahblah`''')
 
     if message.content.startswith('$help'):
         await message.channel.send("usage: `$sh urlofpaper`")
       
 if __name__ == '__main__':
     client.run(os.getenv('DISCORD_TOKEN'))
+
+
         
 
