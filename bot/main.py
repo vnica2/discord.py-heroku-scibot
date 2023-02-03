d692aa0d182e95a36e49b59165ca8451acdbeb28
vnica2 <116927189+vnica2@users.noreply.github.com>
2023-02-02 18:20:48 -0600

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -174,7 +174,7 @@ async def on_message(message):
             contents = download_file(url)
             filename = contents[0]
             with open(filename, 'rb') as fp:
-                await message.channel.send(file=discord.File(fp, filename)
+                await message.channel.send(file=discord.File(fp, filename))
                 print("sent file")
                 sys.stdout.flush()
             if os.path.exists(filename):
